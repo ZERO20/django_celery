b8246eca850773c8cb2d25fd795fddb5785f8725
Edgar de la Cruz <edgar.cruz@xepelin.com>
2022-11-22 21:47:28 -0600

    ref: changed functions by celery tasks

--- apps/network/signals.py
+++ apps/network/signals.py
@@ -1,8 +1,8 @@
-from django.contrib.auth.models import User
-from django.db.models.signals import pre_save, post_save, post_delete
+from django.db.models.signals import post_save
 from django.dispatch import receiver
+
 from .models import Post, Subscriber
-from .functions import notify_new_content, notify_new_subscriber
+from .tasks import notify_new_content_async, notify_new_subscriber_async
 
 
 @receiver(post_save, sender=Post)
@@ -13,7 +13,7 @@ def send_mail_to_subscriber(sender, **kwargs):
     """
     if kwargs.get('created', False):
         post = kwargs.get("instance")
-        notify_new_content(post.id)
+        notify_new_content_async.delay(post.id)
 
 
 @receiver(post_save, sender=Subscriber)
@@ -23,4 +23,4 @@ def new_subscriber(sender, **kwargs):
     """
     if kwargs.get('created', False):
         subscriber = kwargs.get("instance")
-        notify_new_subscriber(subscriber.id)
+        notify_new_subscriber_async.delay(subscriber.id)
